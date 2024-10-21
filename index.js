63c2181b0ff511b8604b2562a985ed5cbe2f46cb
FpRaArNkK <mspark1184@naver.com>
2024-10-21 23:08:55 +0900

    Feat: add database config

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express';
 import { tempRouter } from './src/router/temp-route';
+import { BaseError } from './global/base-error';
+import { responseStatus } from './global/response-status';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+
+app.set('port', process.env.PORT || 3000)
 
 app.use('/temp', tempRouter);
 
-app.listen(port, () => {
-    console.log(`example app listening on port ${port}`);
+app.use((req, res, next) => {
+    const err = new BaseError(responseStatus.NOT_SUPPORTED_URI_ERROR);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV == 'DEV' ? err : {};
+    res.status(err.data.status).send(response(err.data));
+});
+
+
+app.listen(app.get('port'), () => {
+    console.log(`example app listening on port ${app.get('port')}`);
 });
