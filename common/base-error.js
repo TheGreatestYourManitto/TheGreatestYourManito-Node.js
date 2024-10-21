4ed0f7d010e1106e050bef881228ad0f55bd0c36
FpRaArNkK <mspark1184@naver.com>
2024-10-22 01:18:16 +0900

    Refactor: BaseError 함수형으로 재작성

--- common/base-error.js
+++ common/base-error.js
@@ -1,6 +1,5 @@
-export class BaseError extends Error {
-    constructor(data) {
-        super(data.message);
-        this.data = data;
-    }
+export const BaseError = (data) => {
+    const error = new Error(data.message);  // 기본 Error 객체 생성
+    error.data = data;  // 추가 데이터 설정
+    return error;
 };
--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -11,3 +11,19 @@ export const tempException = (req, res, next) => {
     console.log(`/temp/exception/${req.params.flag}`);
     return res.send(baseResponse(responseStatus.SUCCESS, checkFlag(req.params.flag)));
 }
+
+import { createUser } from '../service/user-service.js';
+
+export const createUserController = async (req, res, next) => {
+    try {
+        const { nickname, deviceId } = req.body;
+        if (!nickname || !deviceId) {
+            return res.status(400).json({ message: 'Nickname and deviceId are required' });
+        }
+
+        const userIdentifier = await createUser(nickname, deviceId);
+        res.status(201).json({ result: { userIdentifier } });
+    } catch (error) {
+        next(error);  // Error handling middleware로 넘김
+    }
+};
